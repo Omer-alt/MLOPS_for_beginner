3f2bec4b0978601afeeddfa75485d13430ed15b2
Omer-alt <wafomer912@gmail.com>
2024-09-05 03:09:31 +0000

    Refactor: Structuring the loading process into a class

--- simple-ml-pipeline.py
+++ simple-ml-pipeline.py
@@ -1,29 +1,37 @@
 
-# Lords and explore Iris datasets
 from sklearn import datasets
-import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
-from sklearn.linear_model import LogisticRegression
+import pandas as pd
 
 # Loard and Explore the dataset.
-iris = datasets.load_iris()
+class IrisDataset:
+    
+    def __init__(self):
+        self.iris = datasets.load_iris()
+        
+    def load_dataset(self):
 
-_, ax = plt.subplots()
-scatter = ax.scatter(iris.data[:, 0], iris.data[:, 1], c=iris.target)
-ax.set(xlabel=iris.feature_names[0], ylabel=iris.feature_names[1])
-_ = ax.legend(
-    scatter.legend_elements()[0], iris.target_names, loc="lower right", title="Classes"
-)
-plt.show()
+        # Create a DataFrame for the feature data
+        df = pd.DataFrame(data=self.iris.data, columns=self.iris.feature_names)
 
+        # Add the target labels as a new column
+        df['species'] = self.iris.target
 
+        # Map the target integers to species names using map() for readability
+        target_to_name = {i: name for i, name in enumerate(self.iris.target_names)}
+        df['species_name'] = df['species'].map(target_to_name)
+        print(set(self.iris.target_names))
+        return df
+    
+    def displays_rows(self, number_rows=5):
+        return iris_df.head(number_rows)
 
+          
+if __name__ == "__main__":
+    iris_dataset = IrisDataset()
+    iris_df = iris_dataset.load_dataset()
+    print(iris_df.head())
 
-
-
-
+    
 
 
 
